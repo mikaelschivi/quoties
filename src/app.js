c000955f58ef3de6bfedaa18995681d502d0cb60
bandido nao danca bandido bebe e balanca <mikaelschivi@gmail.com>
2023-09-19 08:21:56 -0300

    css file

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const quoteDisplay = document.querySelector('#quote')
 fetch('http://localhost:3000/')
     .then(response => {return response.json()})
     .then(data => {
-        const quoteItem = `<div><h3>` + data.text + `</h3><p>` + data.author + `</p></div>`
+        const quoteItem = `<div><h1>` + `"` + data.text + `"` +  `</h1><h3>` + data.author + `</h3></div>`
         quoteDisplay.insertAdjacentHTML('beforeend', quoteItem)
     })
     .catch(err => {
